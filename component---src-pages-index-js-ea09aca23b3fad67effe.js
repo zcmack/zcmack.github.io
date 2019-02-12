99138c819ffcf2a5b5fe50aa4c141b8de6c26c01
ChrisMack <ChristopherMack@gmmail.com>
2019-02-11 19:23:18 -0600

    Updates

---
+++ component---src-pages-index-js-ea09aca23b3fad67effe.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{139:function(e,t,a){"use strict";a.r(t);var n=a(0),r=a.n(n),i=(a(143),a(146)),l=(a(155),a(145));t.default=function(){return r.a.createElement(i.a,null,r.a.createElement(l.a,{title:"chris.mack",keywords:["gatsby","application","react"]}),r.a.createElement("h1",null,"it's my only resolution"),r.a.createElement("div",{style:{maxWidth:"300px",marginBottom:"1.45rem"}}))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-ea09aca23b3fad67effe.js.map
